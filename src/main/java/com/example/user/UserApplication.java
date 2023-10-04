2d58a8de34cfbccd70ed302a384b02a9b2bda14f
Kumari Nidhi <knidhi2196@gmail.com>
2023-10-04 23:04:10 +0530

    Add Swagger

--- src/main/java/com/example/user/UserApplication.java
+++ src/main/java/com/example/user/UserApplication.java
@@ -3,7 +3,12 @@ package com.example.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+
 @SpringBootApplication
+@OpenAPIDefinition(info=@Info(title="User APIs", version="1.0", description="User Management"))
 public class UserApplication {
 
 	public static void main(String[] args) {
