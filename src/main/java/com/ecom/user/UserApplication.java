4b296208641965d90e1d899b45735f650de3d91a
Kumari Nidhi <knidhi2196@gmail.com>
2023-10-15 16:29:00 +0530

    cors handled

--- src/main/java/com/ecom/user/UserApplication.java
+++ src/main/java/com/ecom/user/UserApplication.java
@@ -2,10 +2,12 @@ package com.ecom.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-
 
 @SpringBootApplication
 @OpenAPIDefinition(info=@Info(title="User APIs", version="1.0", description="User Management"))
@@ -13,6 +15,16 @@ public class UserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
+	}
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registery) {
+				registery.addMapping("/api/**").allowedOrigins("*").allowedMethods("*").allowedMethods("*");
+			}
+		};
 	}
 
 }
